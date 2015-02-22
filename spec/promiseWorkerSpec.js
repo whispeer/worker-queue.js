dc1fce829aed0b3273215de8657a818efae16ea8
Nils Kenneweg <nk@ovt.me>
2015-02-22 23:24:59 +0100

    some way of testing.

--- spec/promiseWorkerSpec.js
+++ spec/promiseWorkerSpec.js
@@ -1,1 +1,13 @@
+define(["src/index", "bower_components/bluebird/js/browser/bluebird"], function(PromiseWorker, bluebird) {
+	describe('', function() {
+		it('', function (done) {
+			var promiseQueue = new PromiseQueue(4, "testWorker");
+			promiseQueue.schedule(55)
+			promiseQueue.schedule(55);
+			promiseQueue.schedule(55);
+			promiseQueue.schedule(55);
+			promiseQueue.schedule(55);
+		});
+	});
+});
 
